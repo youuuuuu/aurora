eb3a65b02cd5b077d0e0f339078dc5ea09d8cf35
youuuuuu <3773195615@qq.com>
2026-06-06 05:52:15 +0000

    2-2

--- aurora-springboot/src/main/java/com/aurora/util/HTMLUtil.java
+++ aurora-springboot/src/main/java/com/aurora/util/HTMLUtil.java
@@ -4,24 +4,13 @@ import com.github.houbb.sensitive.word.bs.SensitiveWordBs;
 
 public class HTMLUtil {
 
-    // temporary unused
-    private static final SensitiveWordBs sensitiveWordBs = SensitiveWordBs.newInstance()
-            .ignoreCase(true)
-            .ignoreWidth(true)
-            .ignoreNumStyle(true)
-            .ignoreChineseStyle(true)
-            .ignoreEnglishStyle(true)
-            .ignoreRepeat(true)
-            .enableNumCheck(false)
-            .enableEmailCheck(false)
-            .enableUrlCheck(false)
-            .init();
+    private static final SensitiveWordBs sensitiveWordBs = SensitiveWordBs.newInstance().init();
 
     public static String filter(String source) {
         source = source.replaceAll("(?!<(img).*?>)<.*?>", "")
                 .replaceAll("(onload(.*?)=)", "")
                 .replaceAll("(onerror(.*?)=)", "");
-        return deleteHMTLTag(source);
+        return sensitiveWordBs.replace(deleteHMTLTag(source));
     }
 
     public static String deleteHMTLTag(String source) {
